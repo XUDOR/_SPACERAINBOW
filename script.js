87583bfe189b7588dedad3457a626d80bd81adfb
Roderick Shoolbraid <rdxenv@gmail.com>
2024-08-06 16:15:00 -0700

    de-bugged-gradient problem on scale of png/jpg ||-- used svg --

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
 function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove('active'));
-    slides[index].classList.add('active');
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[index].classList.add('active');
+  console.log(`Showing slide ${index}`);
 }
 
 function nextSlide() {
